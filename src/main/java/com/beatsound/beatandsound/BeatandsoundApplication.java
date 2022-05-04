1d680033d9066ae3bf19f10f2adb4c837caaa718
Choi Hyoung Jun <chsam2@ajou.ac.kr>
2022-05-04 15:10:25 +0900

    .

---
+++ src/main/java/com/beatsound/beatandsound/BeatandsoundApplication.java
@@ -0,0 +1,13 @@
+package com.beatsound.beatandsound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatandsoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatandsoundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beatsound/beatandsound/BeatandsoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beatsound.beatandsound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatandsoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
